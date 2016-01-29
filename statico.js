4a82663beecb7748ed19469dca7839ac4e06c231
Chathula Sampath <schathula@gmail.com>
2016-01-29 22:55:10 +0530

    Updated statico.js
    Added new features for 2.0!

--- statico.js
+++ statico.js
@@ -1,6 +1,42 @@
 var fs = require('fs');
+var path = require('path');
+
+function isUrl(s) {
+   var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+   return regexp.test(s);
+}
+
+function setuper(req, res) {
+
+  if (req.url != '/') {
+    var file = req.url.substring(1);
+    var type = path.extname(file);
+
+    if (fs.existsSync(file)) {
+      if (type == '.js' || type == '.css') {
+        if (type == '.js') {
+          res.writeHead(200, { "Content-Type" : "application/javascript" });
+        } else if (type == '.css') {
+          res.writeHead(200, { "Content-Type" : "text/css" });
+        } else if (type == '.html') {
+          res.writeHead(200, { "Content-Type" : "text/html" });
+        }
+
+        statico.use(file, {}, function (data) {
+          res.end(data);
+        });
+
+      }
+    }
+  }
+
+}
 
 var statico = {
+
+  setup : function (req, res) {
+    setuper(req, res);
+  },
 
   use : function (filename, replace, cb) {
 
@@ -9,7 +45,8 @@ var statico = {
       var promise = new Promise(function (resolve, reject) {
         var stream = fs.createReadStream(filename);
         stream.on('data', function (chunk) {
-          resolve(chunk.toString());
+          var str = statico.toString();
+          resolve(str);
         });
       });
 
@@ -18,7 +55,6 @@ var statico = {
 
         var stream = fs.createReadStream(filename);
         stream.on('data', function (chunk) {
-          // replace the data
           var str = statico.replacer(chunk.toString(), replace);
 
           if (typeof cb === 'undefined') {
